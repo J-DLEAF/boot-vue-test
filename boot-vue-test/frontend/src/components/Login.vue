7a4a59063a2c1277ab7fc107926f5dff4038dea1
doyeob <boeyod@gmail.com>
2020-01-16 17:12:55 +0900

    pom set, frontend set

---
+++ boot-vue-test/frontend/src/components/Login.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="login">
+    <h1>로그인 페이지 입니다!.</h1>
+    <!-- <button @click="todo()">click?</button>
+    <router-link :to="'todoapp'">router-link</router-link>-->
+    <form class="login" action="/login" method="POST">
+      <input placeholder="Enter your ID" name="id" id="id" />
+      <br />
+      <input placeholder="Enter your password" name="pass" id="pass" />
+      <br />
+      <!-- <p>여기! [[${msg}]] ${msg}</p> -->
+      <button @click="todo()">click!</button>
+      <button type="submit">Login</button>
+    </form>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+  methods: {
+    todo() {
+      this.$router.push({
+        name: "main" //
+      });
+    }
+  },
+  components: {}
+};
+</script>
+
+
+<style scoped>
+.alert-danger p {
+  color: red;
+}
+</style>
