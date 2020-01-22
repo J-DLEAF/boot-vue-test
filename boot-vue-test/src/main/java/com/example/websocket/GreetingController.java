4fa2b606a096ce7f27bbd02f4ee63f177db2b98c
doyeob <boeyod@gmail.com>
2020-01-22 11:40:07 +0900

    websocket

--- boot-vue-test/src/main/java/com/example/websocket/GreetingController.java
+++ boot-vue-test/src/main/java/com/example/websocket/GreetingController.java
@@ -11,6 +11,7 @@ public class GreetingController {
 	@MessageMapping("/hello")
 	@SendTo("/topic/greetings")
 	public Greeting greeting(HelloMessage message) throws Exception {
+		System.out.println("여기실행!");
 		Thread.sleep(1000); // simulated delay
 		return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
 	}
